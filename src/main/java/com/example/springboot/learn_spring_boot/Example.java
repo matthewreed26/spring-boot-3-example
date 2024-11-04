c24a9ecf431396e96ef97b3b61ad09e1cf04ccff
Matt Reed <matthewreed26@gmail.com>
2024-11-03 21:13:41 -0500

    rest controller with request mapping

---
+++ src/main/java/com/example/springboot/learn_spring_boot/Example.java
@@ -0,0 +1,12 @@
+package com.example.springboot.learn_spring_boot;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Example {
+    private long id;
+}
---
+++ src/main/java/com/example/springboot/learn_spring_boot/ExampleController.java
@@ -0,0 +1,19 @@
+package com.example.springboot.learn_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ExampleController {
+
+    @RequestMapping("/example")
+    public List<Example> retrieveAllExamples() {
+        return Arrays.asList(
+                new Example(1),
+                new Example(2)
+        );
+    }
+}
